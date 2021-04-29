aa72e38b9f6754ce2ac13df7b3b9a2e806ab8776
Matheus Lucas <nerdola.cartola@gmail.com>
2021-04-29 09:51:58 -0300

    Criei a função que adiciona os números para soma

--- add/script.js
+++ add/script.js
@@ -1,5 +1,16 @@
 var results = document.getElementById('results')
+var add_list = []
 
 function addNewNumber() {
-    
+    var number_to_add = Number(document.getElementById("input_number").value)
+    console.log(`${number_to_add}`);
+    add_list.push(number_to_add)
+    console.log(add_list);
+    add_list.sort(function(a,b){
+        if (a > b) {return 1}
+
+        if (a < b) {return -1}
+
+        if (a = b) {return 0}
+    })
 }
