898e2d6b0988c4a2a0ed9456b7f396a9cd50ea85
Matheus Lucas <nerdola.cartola@gmail.com>
2021-04-29 09:38:00 -0300

    Adaptando o html

--- add/script.js
+++ add/script.js
@@ -1,8 +1,5 @@
 var results = document.getElementById('results')
 
-function addButton() {
-    var num1 = Number(document.querySelector('input#data_number1').value)
-    var num2 = Number(document.querySelector('input#data_number2').value)
-    var num3 = num1 + num2
-    results.innerHTML= `${num1} + ${num2} = ${num3}`
+function addNewNumber() {
+    
 }
